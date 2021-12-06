ebe0fba979917bc4a041fc90e694c508e57a5b3a
kurella yaswanth <93900678+yash-9929@users.noreply.github.com>
2021-12-06 11:57:42 +0530

    Add files via upload

---
+++ for .C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main () {
+    int i,n;
+    i=1;
+    printf ("enter the value of n :");
+    scanf ("%d",&n);
+    for (i=1;i<n;++i)
+    {
+        printf ("\n%d",i);
+    }
+    
+}
